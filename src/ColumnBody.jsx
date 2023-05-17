5d28cedf58cc4286ebcff86ced707c7f06eec4a0
constructorlib <constructorlib@gmail.com>
2023-05-18 02:31:06 +0900

    seen some cool ideas, implementing them

--- src/ColumnBody.jsx
+++ src/ColumnBody.jsx
@@ -3,14 +3,33 @@ import CardLeft from "./CardLeft";
 function ColumnBody() {
   return (
     <>
-      <Container fluid>
+      <Container fluid className="body-container">
         <CardLeft />
         <Container>
           <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit, rem
-            incidunt dolorum quo fuga recusandae saepe optio. Nam
-            exercitationem, facere quod enim officia ipsam reprehenderit
-            distinctio animi! Quis, odit saepe.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam,
+            dicta nesciunt? Id magnam fuga ullam, eos adipisci consectetur
+            blanditiis quos consequatur veritatis magni facilis doloremque
+            maiores laudantium ducimus deserunt atque ab. Amet quis, saepe
+            temporibus dicta nulla debitis at dolores eligendi? Repellat, aut
+            eum obcaecati, veritatis id tenetur exercitationem harum nihil quia
+            corrupti temporibus consectetur? Nisi nam quia delectus hic
+            voluptatem similique, reiciendis, quis minima aspernatur cumque
+            libero aperiam cum aliquid odio sunt dolores iure quisquam.
+            Excepturi reiciendis molestiae minus enim rerum quidem dolorum modi
+            porro debitis unde possimus quasi earum harum, facilis, voluptates,
+            saepe adipisci quam eaque aspernatur nam facere est blanditiis?
+            Dolores, nulla vero? Tempora adipisci, expedita dicta id provident,
+            blanditiis eum facere molestiae soluta eveniet inventore, ducimus
+            sequi! Amet corrupti accusantium consequuntur autem illo ratione et
+            nostrum impedit aspernatur beatae, vel sunt in, facere, labore culpa
+            quos ex. Illo, neque quas. Est, labore ipsum voluptate alias
+            voluptatem numquam cum dolore quod incidunt obcaecati, nesciunt sunt
+            voluptas in debitis vel asperiores animi distinctio. Error unde
+            incidunt, nisi eius laudantium sequi quod officia, non, fugit velit
+            aliquid asperiores molestiae cum. Dolor, ea tempore et quia impedit
+            error odio eligendi numquam ipsa ut, voluptas aut libero eum
+            exercitationem temporibus soluta?
           </p>
         </Container>
         <CardLeft />
--- src/header.jsx
+++ src/header.jsx
@@ -4,48 +4,65 @@ import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+// import { FaIcon } from "react-icons/fa";
 
 function Header() {
   return (
     <Navbar bg="light" expand="lg">
-      <Container fluid>
-        <Navbar.Brand href="#">Personal Blog</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: "100px" }}
-            navbarScroll
-          >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            {/* <Nav.Link href="#action2">Link</Nav.Link> */}
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-            {/* <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link> */}
-          </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-        </Navbar.Collapse>
+      <Container className="justify-content-evenly">
+        <Nav>
+          <Navbar.Brand href="#">P</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Nav className="justify-content-center"></Nav>
+        </Nav>
+        <Nav className="ml-auto mr-auto">
+          <Nav.Link href="#action1">Home</Nav.Link>
+          <Nav.Link href="#action1">Experience</Nav.Link>
+          <Nav.Link href="#action1">Projects</Nav.Link>
+          <Nav.Link href="#action1">Contacts</Nav.Link>
+        </Nav>
+
+        <Nav>
+          <button type="button" className="btn btn-info btn-round-g">
+            =
+          </button>
+        </Nav>
       </Container>
     </Navbar>
   );
 }
 
 export default Header;
+
+// <Navbar.Collapse id="navbarScroll">
+//           <Nav
+//             className="me-auto my-2 my-lg-0"
+//             style={{ maxHeight: "100px" }}
+//             navbarScroll
+//           >
+//             <Nav.Link href="#action1">Home</Nav.Link>
+//             {/* <Nav.Link href="#action2">Link</Nav.Link> */}
+//             <NavDropdown title="Link" id="navbarScrollingDropdown">
+//               <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+//               <NavDropdown.Item href="#action4">
+//                 Another action
+//               </NavDropdown.Item>
+//               <NavDropdown.Divider />
+//               <NavDropdown.Item href="#action5">
+//                 Something else here
+//               </NavDropdown.Item>
+//             </NavDropdown>
+//             {/* <Nav.Link href="#" disabled>
+//               Link
+//             </Nav.Link> */}
+//           </Nav>
+//           <Form className="d-flex">
+//             <Form.Control
+//               type="search"
+//               placeholder="Search"
+//               className="me-2"
+//               aria-label="Search"
+//             />
+//             <Button variant="outline-success">Search</Button>
+//           </Form>
+//         </Navbar.Collapse>
