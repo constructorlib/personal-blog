c9403b4f2df5e2571bfd4dbaa8c8996b6c36053c
constructorlib <constructorlib@gmail.com>
2023-05-18 00:55:53 +0900

    ditched vanilla bootstrap, went on react bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
 import "./App.css";
-
+import Header from "./header";
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
