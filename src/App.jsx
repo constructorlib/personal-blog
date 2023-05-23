e3dc971d9b705ef1d232e269cf5b20a02107bf74
constructorlib <constructorlib@gmail.com>
2023-05-23 10:58:46 +0900

    header done for now, cards in main body now

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
-import Header from "./header";
+import Header from "./Header";
+import MainTop from "./MainTop";
 import ColumnBody from "./ColumnBody";
 function App() {
   // const [count, setCount] = useState(0);
@@ -8,7 +9,8 @@ function App() {
   return (
     <>
       <Header />
-      <ColumnBody />
+      <MainTop />
+      {/* <ColumnBody /> */}
     </>
   );
 }
