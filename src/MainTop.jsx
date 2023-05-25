0815194d901de4d284736d281abd471b21214d41
constructorlib <constructorlib@gmail.com>
2023-05-25 10:50:34 +0900

    I forgor

--- src/MainTop.jsx
+++ src/MainTop.jsx
@@ -1,65 +1,23 @@
 import React from "react";
-
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 function MainTop() {
   return (
     <>
-      <div>
-        asd ASD Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
-        dolorum fuga corporis atque ipsam, incidunt corrupti, iure inventore
-        enim veritatis eligendi quae totam maxime modi sapiente porro blanditiis
-        distinctio. Tempora veritatis minima saepe suscipit vero magnam
-        explicabo laboriosam nesciunt eaque ipsum, doloribus sit corporis id
-        ullam cupiditate quod. Quae consequuntur facere a ea praesentium
-        molestias? Eum sapiente voluptatum quis blanditiis adipisci, assumenda
-        ad ea libero! Rerum odit itaque quam expedita dicta id eos autem quis?
-        Neque magnam rerum fugiat, porro molestiae cumque enim dolores
-        doloremque accusamus dolorum vero ratione exercitationem adipisci ut
-        deleniti qui, voluptatibus sunt distinctio amet voluptate obcaecati!
-        Autem, deserunt nesciunt ex debitis enim commodi, aspernatur, vitae in
-        quod id quaerat expedita. Deserunt vitae nulla nesciunt harum non! Porro
-        autem dicta eius maxime error ducimus, laboriosam ex cupiditate nam,
-        unde accusamus dolorem repudiandae quasi molestias. Saepe incidunt
-        consequuntur dicta, voluptates expedita sint voluptas eveniet dolore?
-        Corrupti quod esse provident doloremque nesciunt modi accusamus iste
-        omnis aspernatur, earum beatae minus ipsum saepe consectetur quos rem,
-        maiores deleniti inventore cum atque pariatur sequi! Nobis ipsum fuga
-        odit quasi delectus rerum minus fugiat voluptatem? Nesciunt, eius
-        repellendus qui, consequatur quibusdam maxime nulla cupiditate quo quod
-        ad quisquam odio, nisi cum explicabo fugiat eos aliquam? Voluptatum
-        nobis a deserunt reiciendis. Quae eius iure dolorum debitis amet nihil
-        quisquam qui, odio suscipit assumenda recusandae, possimus obcaecati
-        voluptates sequi eveniet veniam ab reiciendis eligendi laboriosam ea
-        sunt aliquid asperiores! Temporibus iusto laboriosam, velit aspernatur
-        commodi at tempora, accusantium officia doloremque recusandae dolorem!
-        Nesciunt quas eligendi, inventore consectetur soluta, assumenda incidunt
-        quod illo, quae temporibus odio non corrupti blanditiis! Vero delectus
-        aperiam earum officiis, labore consequuntur quae pariatur et sapiente
-        similique. Inventore, incidunt veniam. Dignissimos dolores ipsum officia
-        odit modi, cumque aperiam, excepturi mollitia, nostrum dolorum similique
-        assumenda. Pariatur harum facilis provident enim qui. Eligendi eveniet
-        culpa expedita amet, cum quos autem doloremque consequatur accusamus
-        aut, aliquid, ipsam quo qui. Ipsum omnis minus velit excepturi
-        explicabo, aut deserunt, ab incidunt odit molestias quo sit possimus
-        nesciunt dicta laborum architecto eveniet perferendis impedit sint illum
-        harum qui culpa veritatis? Inventore esse doloribus deserunt quae
-        sapiente veritatis cumque laborum suscipit aut quasi porro quas
-        reiciendis animi veniam corporis, quaerat at ullam ipsa accusantium,
-        eveniet perferendis impedit? Corrupti cumque voluptas consequuntur,
-        saepe repellendus recusandae consequatur earum consectetur ex dicta
-        quasi nisi dolorem exercitationem quaerat! Doloribus eum maiores, porro
-        expedita autem laudantium iure illo possimus! Blanditiis sit, animi
-        consequuntur illo quia maiores architecto magni sed aliquam vitae neque
-        ipsam necessitatibus reprehenderit dolore beatae, odit repudiandae?
-        Neque ratione voluptatem illum quod ipsam quibusdam sed quisquam
-        blanditiis odit ad impedit numquam recusandae totam quae ipsa optio
-        mollitia voluptate, voluptatibus maxime, quam ex cumque dignissimos
-        delectus enim. Cupiditate tempora quas minima quod ut, unde est libero
-        eum earum eaque consequatur veniam recusandae iusto amet, suscipit
-        eligendi labore aperiam dicta alias. Eveniet nesciunt aliquid dicta
-        modi, esse inventore mollitia impedit incidunt culpa magnam ab, id quam
-        placeat. Fugit aliquam hic repellat dolorem nostrum aliquid blanditiis,
-        minima fuga? Similique illo beatae sunt voluptatum enim!
-      </div>
+      <Container>
+        <Row>
+          <Col className="cardMain">
+            1
+            <img src="images/books.jpg" alt="" />
+          </Col>
+          <Col className="cardMain">2</Col>
+          <Col className="cardMain">3</Col>
+          <Col className="cardMain">4</Col>
+          <Col className="cardMain">5</Col>
+          <Col className="cardMain">6</Col>
+        </Row>
+      </Container>
     </>
   );
 }
